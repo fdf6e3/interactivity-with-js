628809325a8ee0b446164f2d9163def507b7db9e
YOUNGHYUN BAE <yhbae97@gmail.com>
2021-12-30 06:25:36 +0900

    remove the guide messages

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,3 @@
-/*Add the JavaScript here for the function billingFunction().
-It is responsible for setting and clearing the fields in Billing Information */
-
 function billingFunction() {
   if (document.getElementById('same').checked){
     document.getElementById('billingName').value = document.getElementById('shippingName').value;
